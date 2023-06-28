55f4e960711e88d7d4a1f4e686ccc1f5b1689946
josuerx12 <josuerx12@gmail.com>
2023-06-28 14:28:37 -0300

    responsive adjusts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <h1>Where in the world?</h1>
       <button onClick={toggleTheme}>
         <CgDarkMode />
-        {theme === "Dark" ? "Light Mode" : "Dark Mode"}
+        {theme === "Dark" ? "Dark Mode" : "Light Mode"}
       </button>
     </header>
   );
