6a72d2cb7e416739d5aa632f05b523e96275c22f
Josuerx12 <josueaze12@gmail.com>
2023-06-27 21:17:46 -0300

    some increments to InfoCountry page.

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,7 +1,71 @@
 import "./Country.sass";
+import { useThemeContext } from "../../context/ThemeContext";
+import { Link } from "react-router-dom";
 
-const Country = () => {
-  return <div>Country</div>;
+const Country = ({ country }) => {
+  const { theme } = useThemeContext();
+  console.log(country);
+  return (
+    <div className={theme === "Dark" ? "countryDark" : "countryLight"}>
+      <img src={country.flags.png} alt={country.flags.alt} />
+      <div className="countryInfos">
+        <h2>{country.name.common}</h2>
+        <div className="Infos">
+          <div className="geoInfos">
+            <p>
+              <span>Native Name: </span>
+              {Object.values(country.name.nativeName)
+                .map((i) => i.common)
+                .join(", ")}
+            </p>
+            <p>
+              <span>Population: </span>
+              {country.population}
+            </p>
+            <p>
+              <span>Region: </span>
+              {country.region}
+            </p>
+            <p>
+              <span>Sub Region: </span>
+              {country.subregion}
+            </p>
+            <p>
+              <span>Capital: </span>
+              {country.capital}
+            </p>
+          </div>
+          <div className="ecoInfos">
+            <p>
+              <span>Top Level Domain: </span>
+              {Object.values(country.tld)
+                .map((i) => i)
+                .join(", ")}
+            </p>
+            <p>
+              <span>Currencies: </span>
+              {Object.values(country.currencies).map((i) => i.name)}
+            </p>
+            <p>
+              <span>Languages: </span>
+              {Object.values(country.languages)
+                .map((i) => i)
+                .join(", ")}
+            </p>
+          </div>
+        </div>
+
+        {country.borders && (
+          <p className="borderCountries">
+            <span>Border Countries: </span>{" "}
+            {Object.values(country.borders).map((i) => (
+              <Link to={`/info/${i}`}>{i}</Link>
+            ))}
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Country;
--- src/pages/countryDetail/CountryDetail.jsx
+++ src/pages/countryDetail/CountryDetail.jsx
@@ -1,19 +1,54 @@
+import './CountryDetail.sass'
 import { useParams, Link } from "react-router-dom";
 import { useFilter } from "../../context/FilterContext";
 import { useThemeContext } from "../../context/ThemeContext";
 import { MdKeyboardBackspace } from "react-icons/md";
+import Country from "../../components/country/Country";
+import { useEffect, useState } from "react";
 
 const CountryDetail = () => {
+  const [currentCoutry, setCurrentCountry] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   let { countryName } = useParams();
+
   const { theme } = useThemeContext();
-  const { filteredCountries, loading } = useFilter();
+  const { filteredCountries } = useFilter();
+
+  async function getCurrentCountry() {
+    try {
+      const currentCoutry = await filteredCountries.filter(
+        (country) => country.name.common === countryName
+      );
+      setCurrentCountry(...currentCoutry);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getCurrentCountry();
+  }, [countryName]);
 
   return (
-    <div>
+    <section className={theme === "Dark" ? "detailDark" : "detailLight"}>
       <Link to="/">
         <MdKeyboardBackspace /> Back
       </Link>
-    </div>
+      {error != null && (
+        <>
+          <h3>
+            Error: Back to the initial page and try again. If error persist try
+            again latter...
+          </h3>
+          <p>Error to loading a page: Error code: {error}</p>
+        </>
+      )}
+      {error === null && loading === true && <p>Loading data of the country</p>}
+      {error === null && loading === false && <Country country={currentCoutry} />}
+    </section>
   );
 };
 
