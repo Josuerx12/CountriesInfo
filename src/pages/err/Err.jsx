eff967161b344b79ee054cca01dd567766d5d536
josuerx12 <josuerx12@gmail.com>
2023-06-28 15:27:03 -0300

    err page increments

--- src/pages/err/Err.jsx
+++ src/pages/err/Err.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
 const Err = () => {
-  return <div>Error page</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Page not Found...</h2>
+      <Link
+        to="/"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: ".4rem",
+          alignItems: "center",
+          color: "#000",
+        }}
+      >
+        <AiOutlineHome /> Home Page <AiOutlineHome />
+      </Link>
+    </div>
+  );
 };
 
 export default Err;
